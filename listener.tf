5b5c0393e4f3a858af0e39df79d68e84b63a0b99
manu <manu@local.com>
2022-05-25 15:08:46 +0530

    Auto Push Commit

--- listener.tf
+++ listener.tf
@@ -1,10 +1,8 @@
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = var.INTERNAL
+  load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
 
-  default_action {
-    type = "fixed-response"
 
     fixed_response {
       content_type = "text/plain"
