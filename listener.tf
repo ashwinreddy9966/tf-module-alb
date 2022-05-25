a9e8a3ff979ab45bc001dec53296816cf0eaa6a9
manu <manu@local.com>
2022-05-25 15:10:34 +0530

    Auto Push Commit

--- listener.tf
+++ listener.tf
@@ -4,7 +4,8 @@ resource "aws_lb_listener" "front_end" {
   port              = "80"
   protocol          = "HTTP"
 
-  
+  default_action {
+    type = "fixed-response"
 
     fixed_response {
       content_type = "text/plain"
