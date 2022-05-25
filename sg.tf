b1bafda764769f19bb12c4feb260b77a30d15016
manu <manu@local.com>
2022-05-25 10:33:08 +0530

    Auto Push Commit

---
+++ sg.tf
@@ -0,0 +1,53 @@
+# Public LB SG
+resource "aws_security_group" "alb_public" {
+  name        = "roboshop-public-alb-${var.ENV}"
+  description = "roboshop-public-alb-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "Allows MySQL Port"
+    from_port   = var.RDS_MYSQL_PORT
+    to_port     = var.RDS_MYSQL_PORT
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, var.WORKSTATION_IP]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name =  "roboshop-redis-sg-${var.ENV}"
+  }
+}
+
+# Private LB SG
+resource "aws_security_group" "alb_private" {
+  name        = "roboshop-mysql-${var.ENV}"
+  description = "roboshop-mysql-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "Allows MySQL Port"
+    from_port   = var.RDS_MYSQL_PORT
+    to_port     = var.RDS_MYSQL_PORT
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, var.WORKSTATION_IP]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name =  "roboshop-redis-sg-${var.ENV}"
+  }
+}
